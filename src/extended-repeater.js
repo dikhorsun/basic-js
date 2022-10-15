bc0c73fbc84e51879922cd883f36c10f357ae13e
dikhorsun <dmitry.horsun@gmail.com>
2022-10-16 00:46:25 +0300

    feat: solution repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  function repeater(str, options) {
+//   let arrAddition = [];
+//   let arrStr = [];
+//   const repeatTimes = options.repeatTimes || 1,
+//       separator = options.separator || '+',
+//       addition = options.addition+'' || "",   
+//       additionRepeatTimes = options.additionRepeatTimes || 1,
+//       additionSeparator = options.additionSeparator || '|'
+
+//       for(let i = 1; i<=additionRepeatTimes; i++){
+//           arrAddition.push(addition)
+//       }
+//       str = str + arrAddition.join(additionSeparator);
+
+//       for(let i = 1; i<=repeatTimes; i++){
+//           arrStr.push(str)
+//       }
+//       str = arrStr.join(separator);
+
+//       return str
+// }
+function repeater(str, options) {
+  let arrAddition = [];
+  let arrStr = [];
+  const repeatTimes = options.repeatTimes || 1,
+      separator = options.separator || '+',
+      addition = options.addition === undefined? "" : options.addition+'',   
+      additionRepeatTimes = options.additionRepeatTimes || 1,
+      additionSeparator = options.additionSeparator || '|'
+
+      for(let i = 1; i<=additionRepeatTimes; i++){
+          arrAddition.push(addition)
+      }
+      str = str + arrAddition.join(additionSeparator);
+
+      for(let i = 1; i<=repeatTimes; i++){
+          arrStr.push(str)
+      }
+      str = arrStr.join(separator);
+
+      return str
 }
 
 module.exports = {
