4691e318ce3cf968ce227470e1e5a7fb995ac122
dikhorsun <dmitry.horsun@gmail.com>
2022-10-16 18:48:55 +0300

    feat: add cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,27 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-//  function repeater(str, options) {
-//   let arrAddition = [];
-//   let arrStr = [];
-//   const repeatTimes = options.repeatTimes || 1,
-//       separator = options.separator || '+',
-//       addition = options.addition+'' || "",   
-//       additionRepeatTimes = options.additionRepeatTimes || 1,
-//       additionSeparator = options.additionSeparator || '|'
 
-//       for(let i = 1; i<=additionRepeatTimes; i++){
-//           arrAddition.push(addition)
-//       }
-//       str = str + arrAddition.join(additionSeparator);
-
-//       for(let i = 1; i<=repeatTimes; i++){
-//           arrStr.push(str)
-//       }
-//       str = arrStr.join(separator);
-
-//       return str
-// }
 function repeater(str, options) {
   let arrAddition = [];
   let arrStr = [];
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+ const data = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    constructor(value){
+        this.value = value;
+    }
+    encrypt(message, key) {
+        if (!message || ! key){
+            throw new Error ('Incorrect arguments!')
+        }
+
+        const str = message.toUpperCase()
+        let keyUpper = key.toUpperCase()
+
+        if (str.length>keyUpper.length){
+            const numberRepeat = Math.ceil(str.length/keyUpper.length)
+            keyUpper = keyUpper.repeat(numberRepeat).slice(0, str.length)
+        }
+
+        let count = 0,
+            res = '';
+
+            for (let i=0; i<str.length; i++){
+                if(data.includes(str[i])){
+                    res = res + data[(data.indexOf(str[i])+data.indexOf(keyUpper[i-count]))%data.length]
+                } else {
+                    res = res + str[i]
+                    count++
+                }
+            }
+            if(this.value === false){
+                res = res.split('').reverse().join('')
+            }
+            return res
+    }
+
+  
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || ! key){
+            throw new Error ('Incorrect arguments!')
+        }
+
+        const str = encryptedMessage.toUpperCase()
+        let keyUpper = key.toUpperCase()
+
+        if (str.length>keyUpper.length){
+            const numberRepeat = Math.ceil(str.length/keyUpper.length)
+            keyUpper = keyUpper.repeat(numberRepeat).slice(0, str.length)
+        }
+
+        let count = 0,
+            res = '';
+
+            for (let i=0; i<str.length; i++){
+                if(data.includes(str[i])){
+                    res +=data[(data.indexOf(str[i]) - data.indexOf(keyUpper[i - count]) + data.length) % data.length];
+                } else {
+                    res = res + str[i]
+                    count++
+                }
+            }
+            if(this.value === false){
+                res = res.split('').reverse().join('')
+            }
+            return res
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
 
 module.exports = {
   VigenereCipheringMachine
